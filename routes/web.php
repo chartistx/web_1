73636379df30b385830d58b16e5e351d3718d79f
chartistx <chartistx@gmail.com>
2024-06-14 14:00:14 +0300

    updating routes and layout template

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,38 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\journalController;
+use App\Http\Controllers\ProfileController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/journal',[journalController::class, 'show']);
+Route::get('/journal/create',[journalController::class, 'create']);
+Route::post('/journal',[journalController::class, 'store']);
+Route::get('/journal/{id}',[journalController::class, 'show_insert']);
+
+// });
+
+//Route::get('/journal/create_symbol',[journalController::class, 'create_symbol']);
+
+//store jouranl data
+
+//Route::post('/journal',[journalController::class, 'store_symbol']);
+
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/journal/create', function () {
+    return view('new_input');
+});
+Route::get('/login', function () {
+    return view('login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard',[journalController::class, 'show'], function () {
+    return view('journal');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
